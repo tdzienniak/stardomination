923efbea0865a312ad7fb1ec0d2c7fc731d531c0
Tymoteusz Dzienniak <dzienniak.tymoteusz@gmail.com>
2013-02-06 16:12:48 +0100

    demo

---
+++ js/ball.js
@@ -0,0 +1,51 @@
+﻿(function (window) {
+	function Ball (x, y, radius, color, velocity, mass) {
+		this.initialize(x, y, radius, color, velocity, mass);
+	}
+
+    Ball.prototype = new createjs.Shape();
+
+    // original Shape's constructor:
+    Ball.prototype.Shape_initialize = Ball.prototype.initialize;
+
+    Ball.prototype.initialize = function (x, y, radius, color, velocity, mass) {
+    	//original Shape's constructor
+    	this.Shape_initialize();
+
+    	this.graphics.beginFill(color).drawCircle(x, y, radius);
+
+    	this.regX = this.x = x;
+    	this.regY = this.y = y;
+    	this.radius = radius;
+    	this.mass = mass;
+
+    	if (velocity instanceof Vector) {
+    		this.velocity = velocity;
+    	} else {
+    		this.velocity = new Vector(velocity);	
+    	}
+    }
+
+    Ball.prototype.move = function (delta) {
+        this.prevX = this.x;
+        this.prevY = this.y;
+    	this.x += delta / 1000 * this.velocity.x;
+    	this.y += delta / 1000 * this.velocity.y;
+    }
+
+    Ball.prototype.moveWithVector = function (vector) {
+    	this.x += vector.x;
+    	this.y += vector.y;
+    }
+
+    Ball.prototype.previousPosition = function () {
+    	return [this.prevX, this.prevY];
+    }
+    
+    Ball.prototype.updatePosition = function (position) {
+    	this.x = position[0];
+    	this.y = position[1];
+    }
+
+    window.Ball = Ball;
+})(window);
---
+++ js/edge.js
@@ -0,0 +1,26 @@
+﻿(function (window) {
+	function Edge (from, to, color) {
+		this.initialize(from, to, color);
+	}
+
+    Edge.prototype = new createjs.Shape();
+
+    // constructor:
+    Edge.prototype.Shape_initialize = Edge.prototype.initialize;
+
+    Edge.prototype.initialize = function (from, to, color) {
+    	//original Shape's constructor
+    	this.Shape_initialize();
+
+    	this.graphics.beginStroke(color).moveTo(from[0], from[1]).lineTo(to[0], to[1]).endStroke();
+
+    	this.fromX = from[0];
+    	this.fromY = from[1];
+    	this.toX = to[0];
+    	this.toY = to[1];
+
+        this.normal = new Vector([this.toX - this.fromX, this.toY - this.fromY]).rotate(90).normalize();
+    }
+
+    window.Edge = Edge;
+})(window);
