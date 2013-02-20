c3ecaed22a663ffeaba8c8f9bb3eb63b11df075b
Tymoteusz Dzienniak <dzienniak.tymoteusz@gmail.com>
2013-02-20 20:33:45 +0100

    Demo updated.

--- js/configuration-screen.js
+++ js/configuration-screen.js
@@ -2,40 +2,39 @@
 sd.screens = sd.screens || {};
 
 sd.screens["configuration-screen"] = (function () {
-	var tabs;
+    function init () {
+        var tabs = zest(".ui-tab");
 
-	function init () {
-		tabs = document.getElementsByClassName("ui-tab");
+        for (var i = 0, len = tabs.length; i < len; i += 1) {
+            tabs[i].addEventListener("click", function (e) {
+                var activeTab = zest(".active-tab")[0];
 
-		for (var i = 0, len = tabs.length; i < len; i += 1) {
-			tabs[i].addEventListener("click", function (e) {
-				//w pętli usuwane są klasy aktywności 
-				for (var i = 0, len = tabs.length; i < len; i += 1) {
-					tabs[i].removeClass('active-tab'); //
-					tabs[i].children["0"].removeClass('active-background');
-					tabs[i].children["1"].firstChild.removeClass('active-background');
+                if (activeTab) {
+                    activeTab.removeClass("active-tab");
+                    activeTab.children["0"].removeClass('active-background');
+                    activeTab.children["1"].firstChild.removeClass('active-background');
+                }
 
-				}
+                this.addClass('active-tab');
+                this.children["0"].addClass('active-background');
+                this.children["1"].firstChild.addClass('active-background');
+            }, false);
+        }
 
-				this.addClass('active-tab');
-				this.children["0"].addClass('active-background');
-				this.children["1"].firstChild.addClass('active-background');
-			}, false);
-		}
+        
 
-		var backBtn = document.getElementById("back-to-menu");
-		backBtn.addEventListener("click", function (e) {
-			sd.Game.showScreen("menu-screen");
-		}, false);
+        //var backBtn = document.getElementById("back-to-menu");
+        zest("#back-to-menu")[0].addEventListener("click", function (e) {
+            sd.Game.showScreen("menu-screen");
+        }, false);
 
-		var battleBtn = document.getElementById("battle");
-		battleBtn.addEventListener("click", function (e) {
-			sd.Game.showScreen("game-screen");
-		}, false);
+        //var battleBtn = document.getElementById("battle");
+        zest("#battle")[0].addEventListener("click", function (e) {
+            sd.Game.showScreen("game-screen");
+        }, false);
+    }
 
-	}
-
-	return {
-		init: init
-	};
+    return {
+        init: init
+    };
 })();
